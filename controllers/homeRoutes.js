5ab14b3eb67dfee639f13b33e79201922a8fdd23
Francesco Albanese <falbanese96@gmail.com>
2022-07-07 10:31:45 -0400

    Added passport functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const passport = require('passport');
 
 router.get('/', (req, res) => {
   res.render('login');
 });
+
+router.post(
+  '/login',
+  withAuth,
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  })
+);
 
 router.get('/signup', (req, res) => {
   res.render('signup');
